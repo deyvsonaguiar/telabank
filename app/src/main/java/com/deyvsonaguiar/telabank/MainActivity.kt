b03e7141dd00f2a6722312716dc9b973773f8cab
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2021-05-12 18:53:27 -0400

    Iniciando projeto

---
+++ app/src/main/java/com/deyvsonaguiar/telabank/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.deyvsonaguiar.telabank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvList: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        iniciaViews()
+    }
+
+    private fun iniciaViews() {
+        TODO("Not yet implemented")
+    }
+}
